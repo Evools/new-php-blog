59eeb70e478f75ab9040bb67656d2387111ec07e
Commander <37365092+Evools@users.noreply.github.com>
2025-03-19 12:14:12 +0600

    add news section and subscription form to homepage

--- pages/index.php
+++ pages/index.php
@@ -7,7 +7,39 @@
   <?php include "include/category.php"; ?>
 
   <div class="container m-auto">
-
+    <h2 class="text-2xl">Новости</h2>
+    <div class="flex items-start gap-2 justify-between">
+      <div>
+        <div>
+          <div>
+            <img src="" alt="">
+          </div>
+          <div>
+            <div>
+              <p>#How to</p>
+              <p>#редактирование фото</p>
+            </div>
+            <h2>
+              Как сделать генеалогическое дерево с семейными фотографиями
+            </h2>
+            <div>
+              <span>9 минут</span>
+              <span>21.12.2022</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>Подпишитесь на новые посты</h2>
+        <input type="email" placeholder="E-mail">
+        <div>
+          <input type="checkbox" name="" id="">
+          <p>
+            Подписываясь на рассылку, вы соглашаетесь на получение от нас рекламной информации по электронной почте и обработку персональных данных в соответсвии с <a href="#">Политикой конфиденциальности.</a>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 
 </main>
